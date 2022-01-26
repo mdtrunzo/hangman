6b67762108161d2ef93d3362f498e173eb6162a2
Matias Daniel Trunzo <matiasdanieltrunzo@MacBook-Pro-de-Matias.local>
2022-01-26 20:47:25 -0300

    second

--- script.js
+++ script.js
@@ -44,5 +44,76 @@ function displayWord(){
     popup.style.display = 'flex'
   }
 }
+//Update the wrong letters
+function updateWrongLetterEl() {
+  //Display wrong letters
+  wrongLettersEl.innerText = `
+    ${wrongLetters.length > 0 ? 'Wrong' : ''}
+    ${wrongLetters.map(letter => `${letter}`)}
+  `
+  //Display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length
+
+    if(index < errors) {
+      part.style.display = 'block'
+    }else{
+      part.style.display = 'none'
+    }
+  })
+  //Check if lost
+  if(wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunately you lost. 😞'
+    popup.style.display = 'flex'
+  }
+}
+
+//Show notification
+function showNotification() {
+  notification.classList.add('show')
+
+  setTimeout(() => {
+    notification.classList.remove('show')
+  }, 2000);
+}
+
+//Keydown letter press
+window.addEventListener('keydown', e => {
+  if(e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key
+
+    if(selectedWord.includes(letter)){
+      if(!correctLetters.includes(letter)) {
+        correctLetters.push(letter)
+
+        displayWord()
+      }else{
+        showNotification()
+      }
+    }else{
+      if(!wrongLetters.includes(letter)){
+        wrongLetters.push(letter)
+
+        updateWrongLetterEl()
+      }else{
+        showNotification()
+      }
+    }
+  }
+})
+//Restart and play again
+playAgainBtn.addEventListener('click', () => {
+  //Empty arrays
+  correctLetters.splice(0)
+  wrongLetters.splice(0)
+
+  selectedWord = words[Math.floor(Math.random() * words.length)]
+
+  displayWord()
+
+  updateWrongLetterEl()
+
+  popup.style.display = 'none'
+})
 
 displayWord()
